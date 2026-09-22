14c6ab46f2a19d88cc563749729ceabe3acfe8a3
sonji3000-cloud <sonji3000@gmail.com>
2026-09-22 16:31:17 +0900

    원-사각형-삼각형 운동으로 이루어지는 전체 흐름 완성

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,3 +1,23 @@
 # 실습 과제 진행
-while True:
+def move_circle():
+    print("circle")
+    # 원을 움직이는 코드 작성
     pass
+
+def move_rectangle():
+    # 사각형을 움직이는 코드 작성
+    print("rectangle")
+    pass
+
+def move_triangle():
+    # 삼각형을 움직이는 코드 작성
+    print("triangle")
+    pass
+
+
+while True:
+    move_circle()
+    move_rectangle()
+    move_triangle()
+    pass
+
